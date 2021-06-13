575fde87f75bf780a29552c605641d864d4fc8b4
AuroraChairez <aurora_chairez@yahoo.com>
2021-06-13 02:38:45 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+// The value of launchReady assigned in the first if/else block gets changed in the second if/else block. Dangerous waters...
+// Since the issue is with launchReady, ONE way to fix the logic error is to use a different variable to store the fuel check result. 
+// Refactor the code to do this. Verify that your change works by updating the console.log statements.
+
+let launchReadyFuel = false;
+let fuelLevel = 17000;
+let crewStatus = true;
+let computerStatus = 'green';
+
+if (fuelLevel >= 20000) {
+   console.log('Fuel level cleared.');
+   launchReadyFuel = true;
+} else if (lauchReadyFuel = !true);{
+   console.log('WARNING: Insufficient fuel!');
+   launchReady = false;
+} else {
+  console.log("Liftoff");
+}
+console.log("launchReady = ", launchReady);
+
+if (crewStatus && computerStatus === 'green'){
+   console.log('Crew & computer cleared.');
+   launchReady = true;
+} else {
+   console.log('WARNING: Crew or computer not ready!');
+   launchReady = false;
+}
+
+console.log("launchReady = ", launchReady);
